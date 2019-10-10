0cd5aa880d3ac8c7183d5257bc9a20619b67e7ee
Gabe Kugelmass <gkord@me.com>
2019-10-10 13:17:14 -0400

    API call working, data displaying on page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state ={
+      country: '',
+      flag: ''
+    }
+  }
+
+componentDidMount() {
+  axios({
+    method: "GET",
+    url: "https://restcountries.eu/rest/v2/all",
+    dataResponse: "json",
+    params: {
+      format: "json"
+    },
+  })
+    .then(res => {
+      console.log(res);
+      this.setState({
+        country: res.data[0].name,
+        flag: res.data[0].flag
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.country}</h1>
+        <img src={this.state.flag} alt="{`Flag of ${this.state.country}`}"/>
+      </div>
+    )
+  }
 }
-
-export default App;
